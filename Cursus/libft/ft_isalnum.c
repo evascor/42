e6eed4087223c560336dcd2a55ca6de772e6b6d2
evascor <evascor@proton.me>
2023-09-22 18:37:17 +0200

    backup 20230922

---
+++ Cursus/libft/ft_isalnum.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evasco-o <evasco-o@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 16:26:24 by evasco-o          #+#    #+#             */
+/*   Updated: 2023/09/22 17:09:08 by evasco-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <libft.h>
+#include <stdio.h>
+
+int	ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
+
+/*int main(void)
+{
+	char character = '1';
+
+	printf("%d", ft_isalnum(character));
+	return 0;
+}*/
