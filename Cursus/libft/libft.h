f1c77d7af63730933380635876207950e00f555e
evascoor <elisabetvasco@gmail.com>
2023-10-06 18:36:00 +0200

    fixed bzero

--- Cursus/libft/libft.h
+++ Cursus/libft/libft.h
@@ -33,5 +33,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 char	*ft_strdup(const char *s1);
+void    ft_bzero(void *b, size_t len);
 
 #endif
