6442fec48fd6cd4845955ab893e5b26312bfaa6a
evascoor <elisabetvasco@gmail.com>
2023-10-08 21:05:44 +0200

    fixed names calloc

--- Cursus/libft/libft.h
+++ Cursus/libft/libft.h
@@ -29,10 +29,12 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 char	*ft_strdup(const char *s1);
 void    ft_bzero(void *b, size_t len);
+void    *ft_calloc(size_t nmemb, size_t size);
 
 #endif
