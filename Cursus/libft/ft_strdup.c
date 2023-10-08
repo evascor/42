1bd3d2a12c9dc0c69b350b0813bc5b4611cac859
evascoor <elisabetvasco@gmail.com>
2023-10-08 21:19:25 +0200

    strdup completed

--- Cursus/libft/ft_strdup.c
+++ Cursus/libft/ft_strdup.c
@@ -10,12 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
 
 // Allocates sufficient memory for a copy of the s1, does the cpy, and returns
 // a pointer to it. The pointer may subsequently be used as an argument to the
 // funtion free.
 // If insufficient memory available, NULL returned and errno set to ENOMEM
 
-/*char	*ft_strdup(const char *s1)
-{}*/
+char	*ft_strdup(const char *s)
+{
+    int     len;
+    int     i;
+    char    *ptr;
+
+    len = ft_strlen(s) + 1;
+    ptr = (char *)malloc(len * sizeof(*ptr));
+    if (ptr == NULL)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        ptr[i] = (char)s[i];
+        i++;
+    }
+    return (ptr);
+}
